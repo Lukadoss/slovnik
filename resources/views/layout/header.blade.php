5bdf4ae40e6490266b907b6ab0f764abce780cb3
Lukado <lukado222@seznam.cz>
2017-05-01 17:31:26 +0200

    db update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -44,12 +44,12 @@
                     if($num>0){?> </a> | <?php } ?>
 
                 <a class="mdl-navigation__link" href="/profile" style="font-size: 24px">{{auth()->user()->name}}</a>
-                <button class="mdl-button mdl-js-button mdl-button--icon">
+                <button id="menu" class="mdl-button mdl-js-button mdl-button--icon">
                     <i class="material-icons">more_vert</i>
                 </button>
 
                 <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-                    for="demo-menu-lower-right">
+                    for="menu">
                     <a class="mdl-menu__item mdl-menu__item--full-bleed-divider" href="/profile/settings">Nastavení</a>
                     <a class="mdl-menu__item" href="/members">Seznam uživatelů</a>
                     <a class="mdl-menu__item mdl-menu__item--full-bleed-divider" href="/districts">Seznam měst</a>
